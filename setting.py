2a3fc6eefe2594bd31d4f793898955806fbbf5e3
zhxh <zhxhcoder@gmail.com>
2020-03-04 14:35:44 +0800

    init

--- setting.py
+++ setting.py
@@ -4,11 +4,11 @@
 friend_dict={
     'alarm_time': '9:30',
     #微信名称(或备注) ps:不能是微信号，不能不能
-    'wechat_name': '木子',
+    'wechat_name': '小同学',
     #所在城市,
-    'city_name': '洛阳',
+    'city_name': '北京',
     # 相识时间
-    'start_date': '2010-9-1',
+    'start_date': '2011-12-1',
     # 短句的最后留言
-    'sweet_words': '                  来自最爱你的我',
+    'sweet_words': '                  来自火星的我',
 }
